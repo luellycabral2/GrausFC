4063636d4ea2d02ea06e9c9266c1af620965a700
Luélly Cabral <luellycabral21@gmail.com>
2020-10-09 21:26:02 -0300

    Adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Fahrenheit;
+            double Celsius  = 97;
+
+            Console.Write("Coloque o valor de Fahrenheit");
+            Fahrenheit = Double.Parse(Console.ReadLine());
+
+            Celsius = (Fahrenheit - 32) / 1.8; 
+            Console.WriteLine(" Fahrenheit para o Celsius = {0}" , Celsius);
+            Console.Write("\n");
+
+            Console.WriteLine("click a tecla ENTER para sair");
+            Console.ReadKey();
+            
+
+
+
+
+
+            
+        }
+    }
+}
